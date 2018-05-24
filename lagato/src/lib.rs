a4674d5827bd24d7c34f01f05b323fd240be4594
Layl <LaylConway@users.noreply.github.com>
2018-05-24 15:56:10 +0200

    Add directional input helper

--- lagato/src/lib.rs
+++ lagato/src/lib.rs
@@ -10,3 +10,44 @@ mod event;
 pub use self::{
     event::{Event},
 };
+
+use {nalgebra::{Vector2}};
+
+pub struct DirectionalInput {
+    pub backward: bool,
+    pub forward: bool,
+    pub left: bool,
+    pub right: bool,
+}
+
+impl DirectionalInput {
+    pub fn new() -> Self {
+        DirectionalInput {
+            backward: false,
+            forward: false,
+            left: false,
+            right: false,
+        }
+    }
+
+    pub fn to_vector(&self) -> Vector2<f32> {
+        let mut input = Vector2::new(0.0, 0.0);
+        if self.backward { input.y += 1.0; }
+        if self.forward { input.y -= 1.0; }
+        if self.left { input.x -= 1.0; }
+        if self.right { input.x += 1.0; }
+
+        input
+    }
+}
+
+pub fn rotate_vector(value: &mut Vector2<f32>, radians: f32) {
+    let sin = radians.sin();
+    let cos = radians.cos();
+
+    let tx = value.x;
+    let ty = value.y;
+
+    value.x = (cos * tx) - (sin * ty);
+    value.y = (sin * tx) + (cos * ty);
+}
