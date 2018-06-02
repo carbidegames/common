8c4019bb1e19e7e7c21f18522857d37799375820
Layl <LaylConway@users.noreply.github.com>
2018-06-02 17:28:14 +0200

    Fix fragment shader's blend affecting alpha

--- blockengine-rendering/src/s_fragment.glsl
+++ blockengine-rendering/src/s_fragment.glsl
@@ -8,6 +8,6 @@ out vec4 o_color;
 void main() {
     vec4 tex = texture(u_texture, v_tex_coord);
     float blend = dot(v_tex_coord-vec2(0.5,0.5), v_tex_coord-vec2(0.5,0.5));
-    o_color = mix(tex, vec4(0.0,0.0,0.0,0.0), blend*1.0);
+    o_color = vec4(mix(tex.rgb, vec3(0.0, 0.0, 0.0), blend*1.0), tex.a);
     //o_color = texture(u_texture, v_tex_coord);
 }
