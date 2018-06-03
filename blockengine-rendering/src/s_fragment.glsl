b0274edac8d9669854ba6a84c501fb0b13744287
Layl <LaylConway@users.noreply.github.com>
2018-06-03 14:44:11 +0200

    Add alpha testing discard to shader

--- blockengine-rendering/src/s_fragment.glsl
+++ blockengine-rendering/src/s_fragment.glsl
@@ -7,7 +7,11 @@ out vec4 o_color;
 
 void main() {
     vec4 tex = texture(u_texture, v_tex_coord);
+
+    if(tex.a < 0.5) {
+        discard;
+    }
+
     float blend = dot(v_tex_coord-vec2(0.5,0.5), v_tex_coord-vec2(0.5,0.5));
     o_color = vec4(mix(tex.rgb, vec3(0.0, 0.0, 0.0), blend*1.0), tex.a);
-    //o_color = texture(u_texture, v_tex_coord);
 }
